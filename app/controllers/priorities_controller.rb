5bce93085d0a2921c8a7a91a12d7d93f0a32af2d
Shad Self <shad@discypher.org>
2017-07-29 22:14:41 -0600

    Remove views from generate scaffold

--- app/controllers/priorities_controller.rb
+++ app/controllers/priorities_controller.rb
@@ -69,6 +69,6 @@ class PrioritiesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def priority_params
-      params.fetch(:priority, {})
+      params.require(:priority).permit(:level)
     end
 end

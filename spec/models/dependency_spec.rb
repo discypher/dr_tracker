c41c585fa7285273a5213c77c19c4786c8838c10
Shad Self <shad.self@pinnacol.com>
2017-11-18 09:52:36 -0700

    Add a few tests

--- spec/models/dependency_spec.rb
+++ spec/models/dependency_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Dependency, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'expected methods' do
+    it { is_expected.to respond_to(:required_system) }
+    it { is_expected.to respond_to(:system) }
+  end
 end
